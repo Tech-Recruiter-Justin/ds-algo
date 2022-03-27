8052db918c33f4c349e80fd847f227bacf16dea4
Justin Choi <justin@Justins-MBP.lan>
2022-03-28 00:21:47 +0800

    [ALGO-0001] binary search

---
+++ src/main/java/net/justinchoi/dsalgo/binary_search/BinarySearch.java
@@ -0,0 +1,42 @@
+package net.justinchoi.dsalgo.binary_search;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BinarySearch {
+
+    public BinarySearch() {}
+
+    public static int binarySearch(List<Integer> numbers, int target) {
+        int lowIndex = 0;
+        int highIndex = numbers.size() - 1;
+
+        if (numbers.isEmpty()) { return -1; }
+        while(lowIndex <= highIndex) {
+            int midIndex = (lowIndex + highIndex) / 2;
+            int currentNumber = numbers.get(midIndex);
+            if (target == currentNumber) { return midIndex; }
+            if (target < currentNumber) {
+                highIndex = midIndex - 1;
+            } else {
+                lowIndex = midIndex + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearchRecursive(List<Integer> numbers, int target) {
+        if (numbers.isEmpty()) return -1;
+        return binarySearchRecursive(numbers, target, 0, numbers.size() - 1);
+    }
+
+    private static int binarySearchRecursive(List<Integer> numbers, int target, int lowIndex, int highIndex) {
+        if (lowIndex > numbers.size() - 1 || highIndex < 0 ) { return -1; }
+        int midIndex = (lowIndex + highIndex) / 2;
+        int currentNumber = numbers.get(midIndex);
+        if (currentNumber == target) { return midIndex; }
+        if (target < currentNumber) { return binarySearchRecursive(numbers, target, lowIndex, midIndex - 1); }
+        else { return binarySearchRecursive(numbers, target, midIndex + 1, highIndex); }
+    }
+
+}
